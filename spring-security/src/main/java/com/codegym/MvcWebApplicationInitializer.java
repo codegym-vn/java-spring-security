eee702094727174bf0625b38434a4da71d3ecc42
tungnk_2711 <tungdemo@gmail.com>
2018-05-30 22:54:04 +0700

    init project

---
+++ spring-security/src/main/java/com/codegym/MvcWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package com.codegym;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebSecurityConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
